e7e802b80c6976178fa69347865334b80d440887
Wisnu Andika <124800648+pendragonnn@users.noreply.github.com>
2025-02-15 12:27:15 +0700

    feat: [news] add splash screen

---
+++ app/src/main/java/com/example/thenewsapp/ui/SplashScreenActivity.kt
@@ -0,0 +1,38 @@
+package com.example.thenewsapp.ui
+
+import android.content.Intent
+import android.os.Bundle
+import android.os.Handler
+import android.os.Looper
+import androidx.activity.enableEdgeToEdge
+import androidx.appcompat.app.AppCompatActivity
+import androidx.appcompat.app.AppCompatDelegate
+import androidx.core.view.ViewCompat
+import androidx.core.view.WindowInsetsCompat
+import com.example.thenewsapp.R
+
+class SplashScreenActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        enableEdgeToEdge()
+        AppCompatDelegate.setDefaultNightMode(AppCompatDelegate.MODE_NIGHT_NO)
+        setContentView(R.layout.activity_splash_screen)
+        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
+            val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
+            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
+            insets
+        }
+
+
+        Handler(Looper.getMainLooper()).postDelayed({
+            goToMainActivity()
+        }, 3000L)
+    }
+
+    private fun goToMainActivity() {
+        Intent(this, NewsActivity::class.java).also {
+            startActivity(it)
+            finish()
+        }
+    }
+}
